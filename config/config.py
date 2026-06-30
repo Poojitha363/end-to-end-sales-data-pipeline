da44d804fc63ae1063524bfb358fa5b54939d02c
Poojitha Indirala <indiralapoojitha@gmail.com>
2026-06-30 09:25:08 +0530

    Updated README and added dashboard screenshots

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 import os
 
 DB_USERNAME = os.getenv("DB_USERNAME", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Poojitha@789")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "YOUR_PASSWORDS")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "sales_db")
